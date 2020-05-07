90b599dc82b30bca5abecf2a9d1d445d01a25deb
louxinghong <louxinghong@xinyunlian.com>
2020-05-07 10:58:49 +0800

    feat：初始化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import components from './components'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import './styles/main.less'
+import './utils/flexible'
 
-Vue.config.productionTip = false;
+// 注册全局组件
+Object.keys(components).forEach(key => {
+  Vue.component(key, components[key])
+})
+
+Vue.use(Vant)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
